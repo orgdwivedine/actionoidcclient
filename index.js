5d6dda9d8ba1cf829e4b2cd8d1304ee749d10b77
Neha Dwivedi <dwivedine@github.com>
2021-05-17 18:34:30 +0530

    Using the new oidc-client package @1.0.9

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 
-/*
-const client_id = core.getInput('client_id', {required: true});
-const client_secret = core.getInput('client_secret', {required: true});*/
-//const username = core.getInput('username', {required: false});
-/*const password = core.getInput('password', {required: true});
-const scope = core.getInput('scope', {required: true}); */
 
-//let data = {'grant_type':"password", 'client_id':client_id, 'username':username, 'password':password, 'scope':scope, 'client_secret':client_secret};
-//console.log(data);
-const fetch = require("node-fetch");
+
 const core = require('@actions/core');
 const id = require('@dwivedine/oidc-client')
 
-async function postData(){
-   const id_token = await id.getIDToken('ghactions', 'HelloWorld123#', '3')
+async function getID(){
+   const id_token = await id.getIDToken('ghactions', 'HelloWorld123#')
    const val = `ID token is ${id_token}`
    core.setOutput('id_token', id_token);
       
 }
 
-postData()
+getID()
   
 
 //core.setOuput('response_json', username);
--- node_modules/@dwivedine/oidc-client/__tests__/main.test.ts
+++ node_modules/@dwivedine/oidc-client/__tests__/main.test.ts
@@ -1,30 +1,20 @@
-import {getIDToken} from '../src/main'
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import { mainModule } from 'node:process'
+import * as get from '../src/main'
+import {HttpClient} from '@actions/http-client'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+test('Get httpclient', () => {
+  let http: HttpClient = new HttpClient('actions/oidc-client')
+  expect(http).toBeDefined()
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
+test('HTTP get request to get token endpoint', async () => {
+  let http: HttpClient = new HttpClient('actions/oidc-client')
+  let res = await http.get(
+    'https://ghactionsoidc.azurewebsites.net/.well-known/openid-configuration'
+  )
+  expect(res.message.statusCode).toBe(200)
 })
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+test('Get token endpoint', async () => {
+  let url: string = await get.getTokenEndPoint()
+  expect(url).toBeDefined()
 })
